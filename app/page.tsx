fddb9aa6385b2319792bdd3f53dda18ccfbc76ab
raithend <37509998+raithend@users.noreply.github.com>
2024-04-01 23:29:06 +0900

    Next.jsにてプロジェクトを立ち上げた。 不要なファイルを削除し、ボタン、アバター、カルーセル、ダークモードを追加した。

---
+++ app/page.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import {
+  Card,
+  CardContent,
+} from "@/components/ui/card"
+import { ModeToggle } from "@/components/ui/mode-toggle"
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+      <div>
+        <Button>Click me</Button>
+      </div>
+      
+      <div>
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+      </div>
+
+      <div>
+      <Carousel className="w-full max-w-xs">
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index}>
+              <div className="p-1">
+                <Card>
+                  <CardContent className="flex aspect-square items-center justify-center p-6">
+                    <span className="text-4xl font-semibold">{index + 1}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+      </div>
+
+      <div>
+       <ModeToggle></ModeToggle>
+      </div>
+
+    </main>
+  );
+}
