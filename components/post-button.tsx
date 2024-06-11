610357874f119a3c2631e1f18b7c3f26e0e8f76c
raithend <37509998+raithend@users.noreply.github.com>
2024-06-11 23:57:35 +0900

    投稿ボタンをポップオーバーとした。 動作に問題がないことを確認した。

--- components/post-button.tsx
+++ components/post-button.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button"
 import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import {
@@ -8,21 +9,76 @@ import {
     CardHeader,
     CardTitle,
   } from "@/components/ui/card"
-import { PlusCircleIcon} from "lucide-react"
-import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+Popover,
+PopoverContent,
+PopoverTrigger,
+} from "@/components/ui/popover"
+  
+import { CameraIcon } from "lucide-react"
 
 export function PostButton() {
     return(
-        <div className="w-80 bg-slate-300">            
-            <div className="flex items-center justify-center">
-                <div className="hidden md:block mt-4">
-                <Button className="p-6">
-                    <PlusCircleIcon className="h-8 w-8 mr-4" />
-                    <div>
-                        投稿
-                    </div>                             
-                </Button>                      
-                </div>      
+        <div className="hidden md:block">            
+            <div className="flex items-center justify-center mt-4">
+                <Popover>
+                    <PopoverTrigger asChild>
+                        <Button  variant="outline" className="p-6">
+                            <CameraIcon className="h-8 w-8" />
+                            <div className="hidden lg:block ml-4">
+                                投稿
+                            </div>
+                        </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-80">
+                        <div className="grid gap-4">
+                            <div className="space-y-2">
+                                <h4 className="font-medium leading-none">投稿を作成</h4>
+                            </div>
+                            <div className="grid gap-2">
+                                <div className="grid grid-cols-3 items-center gap-4">
+                                    <Label htmlFor="picture">画像</Label>
+                                    <Input
+                                        id="picture"
+                                        type="file"
+                                        className="col-span-2 h-8"
+                                    />
+                                </div>
+                                <div className="grid grid-cols-3 items-center gap-4">
+                                    <Label htmlFor="width">コメント</Label>
+                                    <Input
+                                        id="comment"
+                                        defaultValue=""
+                                        className="col-span-2 h-8"
+                                    />
+                                </div>
+                                <div className="grid grid-cols-3 items-center gap-4">
+                                    <Label htmlFor="width">分類</Label>
+                                    <Input
+                                        id="taxonomy"
+                                        defaultValue=""
+                                        className="col-span-2 h-8"
+                                    />
+                                </div>
+                                <div className="grid grid-cols-3 items-center gap-4">
+                                    <Label htmlFor="width">撮影地</Label>
+                                    <Input
+                                        id="location"
+                                        defaultValue=""
+                                        className="col-span-2 h-8"
+                                    />
+                                </div>
+                            </div>
+                            <Button  variant="outline">
+                                <div>
+                                    投稿！
+                                </div>
+                            </Button>
+                        </div>
+                    </PopoverContent>
+                </Popover>     
             </div>
         </div>
     )
