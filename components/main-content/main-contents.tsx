14f36261202abf07eeebf8f7f2838de9b98c0a7b
raithend <37509998+raithend@users.noreply.github.com>
2024-06-15 18:19:34 +0900

    スマートフォン画面用のヘッダーを作成した。

--- components/main-content/main-contents.tsx
+++ components/main-content/main-contents.tsx
@@ -1,10 +1,14 @@
 import { Posts } from "./posts";
 import { PostButton } from "../post-button";
 import { SmartphoneNavbar } from "./smartphone-navbar";
+import { SmartphoneHeader } from "./smartphone-header";
 
 export function MainContents() {
     return(
         <div className="flex flex-col items-center">
+            <div className="block md:hidden w-full">
+                <SmartphoneHeader/>
+            </div>
             <Posts/>
             <div className="block md:hidden w-full fixed bottom-0">
                 <SmartphoneNavbar/>
---
+++ components/main-content/smartphone-header.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import {
+    Card,
+    CardContent,
+  } from "@/components/ui/card"
+import { UserRoundIcon } from "lucide-react"
+import { SearchBox } from "../search-box";
+
+export function SmartphoneHeader() {
+    return(
+        <div className="flex items-center justify-between">
+            <SearchBox/>
+            <Link href="./profile">
+                <Avatar className="h-8 w-8 m-2">
+                    <AvatarImage src="https://github.com/shadcn.png" />
+                    <AvatarFallback><UserRoundIcon/></AvatarFallback>  
+                </Avatar>                
+            </Link>
+        </div>
+    )
+}
